a17b825b36e52ef1b931792827d77ef374032da2
Forall AI <forallai2022@gmail.com>
2022-10-02 22:47:51 -0700

    Step by step: backend: 2. Implement getYoutubeRelatedVideos
    To run it:
    $ cd backend-step-by-step
    $ yarn
    Edit env.sh and fill in your GOOGLE_API_KEY
    $ yarn start
    open browser to http://localhost:3001
    In GQL playground, issue the query:
    
    query {
      getYoutubeRelatedVideos(videoId: "PZmCYeG3uh4")
    }

--- backend-step-by-step/src/youtube.ts
+++ backend-step-by-step/src/youtube.ts
@@ -1,5 +1,7 @@
 import {GraphQLResolveInfo} from 'graphql'
+import axios from 'axios'
 
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
 const DEBUG = process.env.NODE_ENV === 'development'
 
 export interface YoutubeComment {
@@ -64,6 +66,41 @@ export interface YoutubeRelatedVideos {
   nextPageToken: string
 }
 
+const MAX_ANALYZE_YT_COMMENTS_RESULTS_COUNT = 10
+async function fetchYoutubeRelatedVideos(
+  videoId: string,
+  pageTokenArg?: string,
+  maxResultsArg?: number,
+): Promise<YoutubeRelatedVideos> {
+  try {
+    const pageToken = pageTokenArg || ''
+    const maxResults = maxResultsArg || MAX_ANALYZE_YT_COMMENTS_RESULTS_COUNT
+
+    // use axios to getch youtube related videos via youtube api
+    const resp = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?key=${GOOGLE_API_KEY}&textFormat=plainText&part=snippet&type=video&relatedToVideoId=${videoId}&maxResults=${maxResults}&pageToken=${pageToken}`,
+    )
+    const {nextPageToken, items} = resp.data
+    const videos = items
+      // filter to keep only valid items
+      .filter(
+        (item: any) =>
+          item.snippet && item.id && item.id.kind === 'youtube#video' && item.id.videoId,
+      )
+      // map to extract data we need
+      .map((item: any) => {
+        const videoId = item.id.videoId
+        const snippet = item.snippet
+        const {channelId, channelTitle, title, description, thumbnails, publishedAt} = snippet
+        const thumbnail = thumbnails.default.url
+        return {channelId, channelTitle, title, description, publishedAt, videoId, thumbnail}
+      })
+    return {videos, nextPageToken}
+  } catch (e: any) {
+    throw Error('API call failed')
+  }
+}
+
 // get up to maxResults (default = 10) related videos for
 // the video with the given video id starting at the page token (default '')
 export const getYoutubeRelatedVideos = async (
@@ -81,7 +118,6 @@ export const getYoutubeRelatedVideos = async (
       )}, context=${JSON.stringify(context, null, 2)}`,
     )
   }
-  const videos: YoutubeRelatedVideo[] = []
-  const nextPageToken = ''
-  return {videos, nextPageToken}
+  const {videoId, pageToken, maxResults} = args
+  return fetchYoutubeRelatedVideos(videoId, pageToken, maxResults)
 }
